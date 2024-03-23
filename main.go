558cb85cd202fb7a2e20ce1fe49ae61879c41397
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-23 16:36:04 +0900

    fix: main

--- main.go
+++ main.go
@@ -12,17 +12,8 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
-	}
-	if err := run(context.Background(), l); err != nil {
-		log.Printf("failed to terminate server: %v", err)
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %+v", err)
 		os.Exit(1)
 	}
 }
