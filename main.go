081f2c79c18beb893192600812eb7cc0d8fabc9d
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-23 16:48:18 +0900

    fix

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -19,6 +22,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -31,6 +36,8 @@ func run(ctx context.Context) error {
 	log.Printf("start: with %s", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// コンマンドラインで実験
+			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
