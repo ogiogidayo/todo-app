00ef49612961e2715cf08a3293b9ec1d27296216
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-24 23:38:19 +0900

    feat: add task

---
+++ handler/add_task.go
@@ -0,0 +1,52 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/go-playground/validator/v10"
+	"github.com/ogiogidayo/todo-app/entity"
+	"github.com/ogiogidayo/todo-app/store"
+	"net/http"
+	"time"
+)
+
+type AddTask struct {
+	Store      *store.TaskStore
+	Validdator *validator.Validate
+}
+
+func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var b struct {
+		Title string `json:"title" validate:"required"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+		RespondJson(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	err := at.Validdator.Struct(b)
+	if err != nil {
+		RespondJson(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusBadRequest)
+		return
+	}
+
+	t := &entity.Task{
+		Title:   b.Title,
+		Status:  entity.TaskStatusTodo,
+		Created: time.Now(),
+	}
+	id, err := store.Tasks.Add(t)
+	if err != nil {
+		RespondJson(ctx, w, &ErrResponse{
+			Message: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+	rsp := struct {
+		ID int `json:"id"`
+	}{ID: int(id)}
+	RespondJson(ctx, w, rsp, http.StatusOK)
+}
