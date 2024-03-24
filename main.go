4faeb6965bb04eb2f785f3b89e4be7c8033be547
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-24 17:39:10 +0900

    fix: main

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/ogiogidayo/todo-app/config"
-	"golang.org/x/sync/errgroup"
 	"log"
 	"net"
 	"net/http"
@@ -12,11 +10,14 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/ogiogidayo/todo-app/config"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
 	if err := run(context.Background()); err != nil {
-		log.Printf("failed to terminate server: %+v", err)
+		log.Printf("failed to terminated server: %v", err)
 		os.Exit(1)
 	}
 }
@@ -32,22 +33,21 @@ func run(ctx context.Context) error {
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
-	url := fmt.Sprintf("http://localhost:%s", l.Addr().String())
-	log.Printf("start: with %s", url)
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// コンマンドラインで実験
+			// コマンドラインで実験するため
 			time.Sleep(5 * time.Second)
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
 	eg, ctx := errgroup.WithContext(ctx)
-
 	eg.Go(func() error {
 		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
-			return nil
+			return err
 		}
 		return nil
 	})
