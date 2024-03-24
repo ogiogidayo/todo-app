6eaf211639944294cafde2e3319737c17c657e58
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-24 23:18:42 +0900

    test: AssertResponse

--- testutil/handler.go
+++ testutil/handler.go
@@ -3,6 +3,8 @@ package testutil
 import (
 	"encoding/json"
 	"github.com/google/go-cmp/cmp"
+	"io"
+	"net/http"
 	"testing"
 )
 
@@ -19,4 +21,22 @@ func AssertJSON(t *testing.T, want, got []byte) {
 	if diff := cmp.Diff(jw, jg); diff != "" {
 		t.Fatalf("got differs: (-got +want)\n%s", diff)
 	}
+}
+
+func AssertResponse(t *testing.T, got *http.Response, status int, body []byte) {
+	t.Helper()
+	t.Cleanup(func() { _ = got.Body.Close() })
+	gb, err := io.ReadAll(got.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got.StatusCode != status {
+		t.Fatalf("want status %d, but got %d, body %q", status, got.StatusCode, gb)
+	}
+
+	if len(gb) == 0 && len(body) == 0 {
+		return
+	}
+
+	AssertJSON(t, body, gb)
 }
