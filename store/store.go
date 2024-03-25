3c756073de029fb980bc719e9282df5df26480c7
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-25 12:10:36 +0900

    feat: get task

--- store/store.go
+++ store/store.go
@@ -23,6 +23,13 @@ func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
 	return t.ID, nil
 }
 
+func (ts *TaskStore) Get(id entity.TaskID) (*entity.Task, error) {
+	if ts, ok := ts.Tasks[id]; ok {
+		return ts, nil
+	}
+	return nil, ErrNotFound
+}
+
 func (ts *TaskStore) All() entity.Tasks {
 	tasks := make([]*entity.Task, len(ts.Tasks))
 	for i, t := range ts.Tasks {
