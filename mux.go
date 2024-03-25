ba7fede1d69a218531bbcb85b352f634b46a0b7c
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-25 12:01:15 +0900

    fix: mux rooting

--- mux.go
+++ mux.go
@@ -1,12 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-playground/validator/v10"
+	"github.com/ogiogidayo/todo-app/handler"
+	"github.com/ogiogidayo/todo-app/store"
+	"net/http"
+)
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		_, _ = w.Write([]byte(`{"Status: " ok"}`))
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+
+	at := &handler.AddTask{Store: store.Tasks, Validdator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }
