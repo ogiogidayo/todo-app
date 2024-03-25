4caf53fc5b5fe35e5108bd7367dfa74400c2e509
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-03-25 16:47:37 +0900

    feat: add AddTask

--- store/task.go
+++ store/task.go
@@ -16,3 +16,25 @@ func (r *Repository) ListTasks(ctx context.Context, db Queryer) (entity.Tasks, e
 	}
 	return tasks, nil
 }
+
+func (r *Repository) AddTask(ctx context.Context, db Execer, t *entity.Task) error {
+	t.Created = r.Clocker.Now()
+	t.Modified = r.Clocker.Now()
+	sql := `INSERT INTO task
+		(title, status, created, modified)
+	VALUES (?, ?, ?, ?)`
+	result, err := db.ExecContext(
+		ctx, sql, t.Title, t.Status,
+		t.Created, t.Modified,
+	)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	t.ID = entity.TaskID(id)
+
+	return nil
+}
