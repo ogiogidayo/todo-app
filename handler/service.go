a7f57e14b073cdf645e50e962153c423dfad6dc0
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-03 11:13:19 +0900

    chore: go generate

---
+++ handler/service.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	"context"
+	"github.com/ogiogidayo/todo-app/entity"
+)
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . ListTaskService AddTaskService
+type ListTaskService interface {
+	ListTask(ctx context.Context) (entity.Tasks, error)
+}
+
+type AddTaskService interface {
+	AddTask(ctx context.Context, title string) (*entity.Task, error)
+}
