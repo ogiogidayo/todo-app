b1f9dcd7dc6ebd02c50cfb1d26f3a8d79b5f45ef
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-06 17:44:41 +0900

    feat: add parse key

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,7 +1,12 @@
 package auth
 
 import (
+	"context"
 	_ "embed"
+	"fmt"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/ogiogidayo/todo-app/clock"
+	"github.com/ogiogidayo/todo-app/domain"
 )
 
 //go:embed cert/secret.pem
@@ -9,3 +14,37 @@ var rawPrivKey []byte
 
 //go:embed cert/public.pem
 var rawPubKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store                 Store
+	Clocker               clock.Clocker
+}
+
+type Store interface {
+	Save(ctx context.Context, key string, userID domain.UserID) error
+	Load(ctx context.Context, key string) (domain.UserID, error)
+}
+
+func NewJWTer(s Store) (*JWTer, error) {
+	j := &JWTer{Store: s}
+	privKey, err := parse(rawPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: %w", err)
+	}
+	pubKey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("faild in NewJWTer: %w", err)
+	}
+	j.PrivateKey = privKey
+	j.PublicKey = pubKey
+	j.Clocker = clock.RealClocker{}
+	return j, nil
+}
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
