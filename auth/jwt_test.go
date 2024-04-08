62039fe51627dc7e8219389eaedd894effb8c6f5
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-08 16:16:33 +0900

    feat: add jwt Test

--- auth/jwt_test.go
+++ auth/jwt_test.go
@@ -2,6 +2,10 @@ package auth
 
 import (
 	"bytes"
+	"context"
+	"github.com/ogiogidayo/todo-app/clock"
+	"github.com/ogiogidayo/todo-app/domain"
+	"github.com/ogiogidayo/todo-app/testutil/fixture"
 	"testing"
 )
 
@@ -13,5 +17,29 @@ func TestEmbed(t *testing.T) {
 	want = []byte("-----BEGIN PRIVATE KEY-----")
 	if !bytes.Contains(rawPrivKey, want) {
 		t.Errorf("want %s, but got %s", want, rawPrivKey)
+	}
+}
+
+func TestJWTer_GenerateToken(t *testing.T) {
+	ctx := context.Background()
+	moq := &StoreMock{}
+	wantID := domain.UserID(20)
+	u := fixture.User(&domain.User{ID: wantID})
+	moq.SaveFunc = func(ctx context.Context, key string, userID domain.UserID) error {
+		if userID != wantID {
+			t.Errorf("want %d, but got %d", wantID, userID)
+		}
+		return nil
+	}
+	sut, err := NewJWTer(moq, clock.RealClocker{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := sut.GenerateToken(ctx, *u)
+	if err != nil {
+		t.Fatalf("not want err: %v", err)
+	}
+	if len(got) == 0 {
+		t.Errorf("token is empty")
 	}
 }
