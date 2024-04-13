026fcf46c9949f84881fa9e99f5709127a4be038
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-13 22:21:54 +0900

    feat: add GetUser

--- database/user.go
+++ database/user.go
@@ -31,3 +31,14 @@ func (r *Repository) RegisterUser(ctx context.Context, db Execer, u *domain.User
 	u.ID = domain.UserID(id)
 	return nil
 }
+
+func (r *Repository) GetUser(ctx context.Context, db Queryer, name string) (*domain.User, error) {
+	u := &domain.User{}
+	sql := `SELECT
+		id, name, password, role, created, modified
+		FROM user WHERE name = ?`
+	if err := db.GetContext(ctx, u, sql, name); err != nil {
+		return nil, err
+	}
+	return u, nil
+}
