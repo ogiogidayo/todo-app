954a79720664a95990c906a00ad566b9b2fb0fbd
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-04-14 11:48:30 +0900

    feat: add AuthMiddleware

---
+++ handler/middleware.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"github.com/ogiogidayo/todo-app/auth"
+	"net/http"
+)
+
+func AuthMiddleware(j *auth.JWTer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			req, err := j.FillContext(r)
+			if err != nil {
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "not find auth info",
+					Details: []string{err.Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
